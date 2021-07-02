5999f55063c55cd8cea3186e2a834ed681371df0
emrulovd <denisemrulov998@gmail.com>
2021-07-02 14:08:48 +0300

    Modifications 02/07/2021: Adding Input UI component Styling Input component

--- src/components/Auth/Authenticate/Authenticate.js
+++ src/components/Auth/Authenticate/Authenticate.js
@@ -1,16 +1,20 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useRef } from 'react';
 
 import logoBlack from '../../../assets/dna-black-logo-symbol.png';
 import classes from './Authenticate.module.css';
 
 import Card from '../../UI/Card/Card';
+import Input from '../../UI/Input/Input';
 
 const Authenticate = () => {
+    const usernameRef = useRef();
     return(
         <Fragment>
             <img className={classes.Logo} src={logoBlack} alt=".."/>
                 <Card className={classes.CardDesign}>
                     <h3 className={classes.Title}>Login</h3>
+                    <Input elementType="input" name="username" ref={usernameRef} label="username" type="text"/>
+                    <Input elementType="input" name="password" ref={usernameRef} label="password" type="password"/>
                 </Card>
         </Fragment>
     )
