50b555cfdf6d2de5206bdfaec7a589dbd34c1772
emrulovd <denisemrulov998@gmail.com>
2021-07-02 11:48:58 +0300

    Modifications 02/07/2021: Adding Card UI component Adding style to the Card component

--- src/components/Auth/Authenticate/Authenticate.js
+++ src/components/Auth/Authenticate/Authenticate.js
@@ -1,11 +1,17 @@
 import React, { Fragment } from 'react';
 
 import logoBlack from '../../../assets/dna-black-logo-symbol.png';
+import classes from './Authenticate.module.css';
+
+import Card from '../../UI/Card/Card';
 
 const Authenticate = () => {
     return(
         <Fragment>
-            <img className={} src={logoBlack} alt=".."/>
+            <img className={classes.Logo} src={logoBlack} alt=".."/>
+                <Card className={classes.CardDesign}>
+                    <h3 className={classes.Title}>Login</h3>
+                </Card>
         </Fragment>
     )
 }
