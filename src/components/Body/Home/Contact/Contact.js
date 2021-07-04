0ab110cc94420db66db6cad2814f9145e8e8e1d2
emrulovd <denisemrulov998@gmail.com>
2021-07-04 18:42:19 +0300

    Modifications 04/07/2021: Adding contact component

---
+++ src/components/Body/Home/Contact/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Aos from 'aos';
+
+import { Container, Row, Col } from 'react-bootstrap';
+import classes from './Contact.module.css';
+
+
+const Contact = () => {
+    return(
+        <Container>
+            <Row>
+                <Col>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Contact;
