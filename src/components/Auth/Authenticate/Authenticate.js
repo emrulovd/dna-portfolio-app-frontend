0696d5c61665e117c25d2fdc412c8fb44eff1017
emrulovd <denisemrulov998@gmail.com>
2021-07-07 10:50:15 +0300

    Modifications 07/07/2021: Adding auth form

--- src/components/Auth/Authenticate/Authenticate.js
+++ src/components/Auth/Authenticate/Authenticate.js
@@ -1,26 +1,47 @@
-import React, { Fragment, useRef } from 'react';
+import React, { useRef } from "react";
 
-import logo from '../../../assets/dna-code-white.png';
-import classes from './Authenticate.module.css';
+import logo from "../../../assets/dna-code-white.png";
+import classes from "./Authenticate.module.css";
 
-import Card from '../../UI/Card/Card';
-import Input from '../../UI/Input/Input';
-import Button from '../../UI/Button/Button';
+import Card from "../../UI/Card/Card";
+import Input from "../../UI/Input/Input";
+import Button from "../../UI/Button/Button";
 
 const Authenticate = () => {
-    const usernameRef = useRef();
-    return(
-        <Fragment>
-            <div className={classes.Wrapper}>
-                <Card>
-                    <img className={classes.Logo} src={logo} alt=".."/>
-                    <Input elementType="input" name="username" reff={usernameRef} label="username" type="text" />
-                    <Input elementType="input" name="password" reff={usernameRef} label="password" type="password"/>
-                    <Button>Login</Button>
-                </Card>
-            </div>
-        </Fragment>
-    )
-}
+  const usernameInputRef = useRef();
+  const passwordInputRef = useRef();
+
+  const authenticateHandler = (event) => {
+    console.log(event)
+    event.preventDefault();
+    console.log(usernameInputRef);
+    console.log(passwordInputRef);
+  };
+
+  return (
+    <div className={classes.Wrapper}>
+      <Card>
+        <img className={classes.Logo} src={logo} alt=".." />
+        <form onSubmit={(event) => authenticateHandler(event)}>
+          <Input
+            elementType="input"
+            name="username"
+            reff={usernameInputRef}
+            label="username"
+            type="text"
+          />
+          <Input
+            elementType="input"
+            name="password"
+            reff={passwordInputRef}
+            label="password"
+            type="password"
+          />
+          <Button type="submit" >Login</Button>
+        </form>
+      </Card>
+    </div>
+  );
+};
 
 export default Authenticate;
