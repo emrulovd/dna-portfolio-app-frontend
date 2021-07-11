059a5df1466f71bda5edf0b7dcae6d9e9115bdc9
emrulovd <denisemrulov998@gmail.com>
2021-07-11 21:08:30 +0300

    Modifications 11/07/2021: Adding new UI element (Modal)

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,25 +1,43 @@
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 
-import classes from './Modal.module.css';
+import Card from "../Card/Card";
+import Button from "../Button/Button";
+import classes from "./Modal.module.css";
 
 const Backdrop = (props) => {
-  return <div className={classes.Backdrop}></div>;
+  return <div className={classes.backdrop}  onClick={props.onConfirm}></div>;
 };
 
 const ModalOverlay = (props) => {
-  return <Fragment></Fragment>;
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.onConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
 };
 
 const Modal = (props) => {
   return (
     <Fragment>
       {ReactDOM.createPortal(
-        <Backdrop />,
+        <Backdrop onConfirm={props.onConfirm} />,
         document.getElementById("backdrop-root")
       )}
       {ReactDOM.createPortal(
-        <ModalOverlay />,
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
         document.getElementById("overlay-root")
       )}
     </Fragment>
