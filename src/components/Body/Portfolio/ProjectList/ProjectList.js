3e85d136e859f5937489200b8c8d3079345516d4
emrulovd <denisemrulov998@gmail.com>
2021-07-16 19:25:45 +0300

    Changes;

---
+++ src/components/Body/Portfolio/ProjectList/ProjectList.js
@@ -0,0 +1,11 @@
+import classes from './ProjectList.module.css';
+
+const ProjectList = () => {
+    return(
+        <div className={classes.Wrapper}>
+            <h3 className={classes.Title}>Projetcts</h3>
+        </div>
+    )
+}
+
+export default ProjectList;
