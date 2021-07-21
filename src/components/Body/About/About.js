42e62a59fddeffa15874cc6c741ebe68f5d87ead
emrulovd <denisemrulov998@gmail.com>
2021-07-21 10:42:33 +0300

    Modifications

--- src/components/Body/About/About.js
+++ src/components/Body/About/About.js
@@ -1,11 +1,13 @@
 import classes from './About.module.css';
 
 import AboutMain from './AboutMain/AboutMain';
+import AboutGoals from './AboutGoals/AboutGoals';
 
 const About = () => {
   return (
     <div className={classes.Wrapper}>
         <AboutMain/>
+        <AboutGoals/>
     </div>
   );
 };
